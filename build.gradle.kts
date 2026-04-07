56710ef6e08fec6bbd490755fd46cb4fadcb0ffa
MonkeyMoon104 <lollolilo32@gmail.com>
2026-04-07 16:37:15 +0200

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,30 @@
+plugins {
+    id("java-library")
+    id("com.gradleup.shadow") version "9.4.1"
+}
+
+repositories {
+    mavenCentral()
+    maven("https://repo.papermc.io/repository/maven-public/")
+}
+
+dependencies {
+    compileOnly("io.papermc.paper:paper-api:1.21.11-R0.1-SNAPSHOT")
+}
+
+java {
+    toolchain.languageVersion = JavaLanguageVersion.of(21)
+}
+
+tasks {
+    build {
+        dependsOn(shadowJar)
+    }
+
+    processResources {
+        val props = mapOf("version" to version , "description" to project.description )
+        filesMatching("plugin.yml") {
+            expand(props)
+        }
+    }
+}
