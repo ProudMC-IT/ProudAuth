4eda87791ea35e4463781f42c29d8c87f71bb648
MonkeyMoon104 <lollolilo32@gmail.com>
2026-04-24 02:19:52 +0200

    feat(storage): expose trusted IP lookup and upsert APIs

--- common/src/main/java/com/monkey/proudAuth/common/storage/StorageProvider.java
+++ common/src/main/java/com/monkey/proudAuth/common/storage/StorageProvider.java
@@ -4,6 +4,7 @@ import com.monkey.proudAuth.common.config.ProudAuthSettings;
 
 public interface StorageProvider extends
         AccountStorage,
+        TrustedIpStorage,
         SessionStorage,
         BackendJoinProbeStorage,
         BridgeAssertionStorage,
